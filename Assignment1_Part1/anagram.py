4bc18256ea06dcaef91d1c721c6b8864eb642411
Sachin Pandit <spandit@usc.edu>
2016-01-28 13:28:13 -0800

    NLP: Assignment1-part: Final revision

--- Assignment1_Part1/anagram.py
+++ Assignment1_Part1/anagram.py
@@ -5,20 +5,21 @@ import sys
 global output
 output = []
 
-def anagram (string_array):
+def anagram (string_array, fd):
     if [] == string_array or string_array == None:
         return
 
     if 1 == len(string_array):
         output.append (string_array[0])
-        print ''.join(output)
+        fd.write (''.join(output))
+        fd.write ("\n")
         output.pop()
         return
 
     for index in range (0, len(string_array)):
         output.append (string_array[index])
         temp = string_array[:index] + string_array[index+1:]
-        anagram (temp)
+        anagram (temp, fd)
         output.pop()
 
 
@@ -27,10 +28,11 @@ def main():
         print "Syntax : ./anagram.py <string1>"
         exit()
 
+    fd = open ("anagram_out.txt", "w");
     string_length = len (sys.argv[1]);
     string_array = list (sorted(sys.argv[1]));
 
-    anagram (string_array);
+    anagram (string_array, fd);
 
 if __name__ == "__main__":
     main()
