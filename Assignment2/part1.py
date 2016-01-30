ed6d6b2d4701af33a1c730945cbe24db12077201
Sachin Pandit <spandit@usc.edu>
2016-01-30 02:15:47 -0800

    NLP: Assignment2 - First part is donegit add part1.py  I guess...

--- Assignment2/part1.py
+++ Assignment2/part1.py
@@ -3,11 +3,72 @@
 import sys
 import os
 import string
+import collections
 
 truth_pos = {}
 truth_neg = {}
 decep_pos = {}
 decep_neg = {}
+
+truth_pos_wordcount = 0
+truth_neg_wordcount = 0
+decep_pos_wordcount = 0
+decep_neg_wordcount = 0
+
+total_unique_count = 0
+total_keys = {}
+
+
+def write_output():
+    fwrite = open ("output.txt", "w")
+    length = len (total_keys.keys())
+    for i in total_keys:
+        if (i in truth_pos):
+            prob1 = (truth_pos[i] + 1) / (truth_pos_wordcount + total_unique_count)
+        else:
+            prob1 = 0
+
+        if (i in truth_neg):
+            prob2 = (truth_neg[i] + 1) / (truth_neg_wordcount + total_unique_count)
+        else:
+            prob2 = 0
+
+        if (i in decep_pos):
+            prob3 = (decep_pos[i] + 1) / (decep_pos_wordcount + total_unique_count)
+        else:
+            prob3 = 0
+
+        if (i in decep_neg):
+            prob4 = (decep_neg[i] + 1) / (decep_neg_wordcount + total_unique_count)
+        else:
+            prob4 = 0
+        str1 = i + " " + str(prob1) + " " + str(prob2) + " "
+        str1 += str(prob3) + " " + str(prob4) + str("\n")
+        fwrite.write (str1)
+
+
+def get_unique_count():
+    global total_keys
+    global total_unique_count
+
+    total_keys = collections.Counter (truth_pos) + collections.Counter (truth_neg) + collections.Counter (decep_pos) + collections.Counter (decep_neg)
+    total_unique_count = len (total_keys.keys())
+    print ("Total unique word count = %d" % (total_unique_count))
+
+
+def class_wordcount ():
+    global truth_pos_wordcount
+    global truth_neg_wordcount
+    global decep_pos_wordcount
+    global decep_neg_wordcount
+
+    truth_pos_wordcount = sum (truth_pos.values())
+    truth_neg_wordcount = sum (truth_neg.values())
+    decep_pos_wordcount = sum (decep_pos.values())
+    decep_neg_wordcount = sum (decep_neg.values())
+    print ("LISTENNNNN LISTENNNNN LISTENNNNNL:\nTruthful positive = %d\nTruthful negative = %d\nDeceptive positive = %d\nDeceptive Negative = %d\n" % (truth_pos_wordcount, truth_neg_wordcount, decep_pos_wordcount, decep_neg_wordcount))
+
+
 
 def check_and_increment (dict_var, word):
     #print ("Word = %s" % word)
@@ -17,6 +78,11 @@ def check_and_increment (dict_var, word):
         dict_var[word] = 1
 
 def print_dictionary ():
+    global truth_neg
+    global decep_neg
+    global truth_pos
+    global decep_pos
+
     print ("TRUTHFUL NEGATIVE------------------------------\n")
     for i in truth_neg:
         if (len (i) != 0):
@@ -37,7 +103,13 @@ def print_dictionary ():
         if (len (i) != 0):
             print ("%s : %s" % (i, decep_neg[i]))
 
+
 def generate_dictionary (word, case_number):
+    global truth_neg
+    global decep_neg
+    global truth_pos
+    global decep_pos
+
     if (case_number == 1):
         check_and_increment (truth_neg, word)
     elif (case_number == 2):
@@ -67,6 +139,7 @@ def read_file (filename, case_number):
 def process_filename(subdir, file):
     if (file != ".DS_Store"):
         temp = os.path.join (subdir, file)
+        print ("File = %s" % (temp))
         if (("negative" in temp) and ("truthful" in temp)):
             print ("\n%s is %s and %s" % (file, "negative", "truthful"))
             read_file (temp, 1)
@@ -74,12 +147,15 @@ def process_filename(subdir, file):
         if (("negative" in temp) and ("deceptive" in temp)):
             print ("\n%s is %s and %s" % (file, "negative", "deceptive"))
             read_file (temp, 2)
+            #print_dictionary ()
         if (("positive" in temp) and ("truthful" in temp)):
             print ("\n%s is %s and %s" % (file, "positive", "truthful"))
             read_file (temp, 3)
+            #print_dictionary ()
         if (("positive" in temp) and ("deceptive" in temp)):
             print ("\n%s is %s and %s" % (file, "positive", "deceptive"))
             read_file (temp, 4)
+            #print_dictionary ()
 
 def main():
     for subdir, dirs, files in os.walk ("op_spam_train"):
@@ -87,6 +163,9 @@ def main():
             for file in files:
                 process_filename (subdir, file)
 
+    class_wordcount()
+    get_unique_count()
+    write_output()
     #print_dictionary ()
 
 if __name__ == "__main__":
