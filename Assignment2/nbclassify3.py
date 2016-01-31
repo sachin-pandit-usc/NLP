5d5d6290c8116a5dc58a37f06009f840a2d8f53b
Sachin Pandit <spandit@usc.edu>
2016-01-31 01:04:46 -0800

    NLP: Assignment2 - Intermediate stage!

---
+++ Assignment2/nbclassify3.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python3 -tt
+
+import sys
+import os
+import math
+import string
+
+word_dict = {}
+
+def write_output (subdir, file, prob1, prob2, prob3, prob4):
+    fwrite = open ("nboutput.txt", "w")
+    max_value = max (prob1, prob2, prob3, prob4)
+
+    if (prob1 == max_value):
+        temp = ""
+        temp += "truthful" + " " + "positive" + os.path.join (subdir, file) + "\n"
+        fwrite.write (temp)
+    elif (prob2 == max_value):
+        temp = ""
+        temp += "truthful" + " " + "negative" + os.path.join (subdir, file) + "\n"
+        fwrite.write (temp)
+    elif (prob3 == max_value):
+        temp = ""
+        temp += "deceptive" + " " + "positive" + os.path.join (subdir, file) + "\n"
+        fwrite.write (temp)
+    elif (prob4 == max_value):
+        temp = ""
+        temp += "deceptive" + " " + "negative" + os.path.join (subdir, file) + "\n"
+        fwrite.write (temp)
+
+
+
+def calculate_prob (word, prob1, prob2, prob3, prob4):
+    print ("Word = %s\n" % word)
+    if word in word_dict:
+        temp = word_dict[word]
+        count = 0
+        for buf in temp.split():
+            if (count == 0):
+                prob1 += math.log (float(buf))
+            elif (count == 1):
+                prob2 += math.log (float(buf))
+            elif (count == 2):
+                prob3 += math.log (float(buf))
+            elif (count == 3):
+                prob4 += math.log (float(buf))
+            count += 1
+
+
+def process_filename (subdir, file):
+    try:
+        prob1 = 0
+        prob2 = 0
+        prob3 = 0
+        prob4 = 0
+        if (file != ".DS_Store"):
+            filepath = os.path.join (subdir, file)
+            #print ("%s\n" % (file))
+            fd1 = open (filepath, "r")
+            for line in fd1:
+                for word in line.split():
+                    for c in string.punctuation:
+                        word = word.replace (c,"")
+                    word = word.lower()
+                    calculate_prob (word, prob1, prob2, prob3, prob4)
+
+            write_output (subdir, file, prob1, prob2, prob3, prob4)
+    except FileNotFoundError:
+        print ("File not found\n")
+
+def traverse_file():
+    for subdir, dirs, files in os.walk (sys.argv[1]):
+        if (len(files) != 0):
+            for file in files:
+                process_filename (subdir, file)
+
+
+def print_classification():
+    for i in word_dict:
+        print ("%s -> %s\n" % (i, word_dict[i]))
+
+
+def read_model_file ():
+    try:
+        fd = open ("nbmodel.txt", "r")
+        for line in fd:
+            count = 0
+            word_buf = ""
+            for word in line.split():
+                if (count == 0):
+                    word_buf = word
+                    word_buf_second = ""
+                else:
+                    word_buf_second += " " + word
+                if (count == 4 and len(word_buf) != 0):
+                    word_dict[word_buf] = word_buf_second
+                count += 1
+    except FileNotFoundError:
+        print ("File does not exist\n")
+
+
+def check_syntax():
+    if (len (sys.argv) != 2):
+        print ("Syntax: ./nbclassify <file_path>")
+        exit()
+
+def main():
+    check_syntax ()
+    read_model_file()
+    #print_classification()
+    traverse_file()
+
+if __name__ == "__main__":
+    main()
