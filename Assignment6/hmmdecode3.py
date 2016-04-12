1ef100ed8db49adcdb2ef3e790e6fa13726fac36
Sachin Pandit <sachinspandits@gmail.com>
2016-04-12 01:20:39 -0700

    NLP: Assignment6: completed with 84% accuracy

--- Assignment6/hmmdecode3.py
+++ Assignment6/hmmdecode3.py
@@ -74,11 +74,22 @@ def read_model_file (fd):
             key2 = words[2].strip()
             value = int(words[3].strip())
             if key1 in tag_dict:
-                trans_dict[key1][key2] = math.log(value/tag_dict[key1])
+                trans_dict[key1][key2] = math.log((value+1)/(tag_dict[key1]+len(tag_dict)))
             if key1 == "start_state_q0":
-                trans_dict[key1][key2] = math.log(value/line_count)
+                trans_dict[key1][key2] = math.log((value+1)/(line_count+len(tag_dict)))
         elif "5" == flag:
             line_count = int(words[1].strip())
+
+
+def smoothing():
+    for tag in end_tag_dict:
+        if tag not in trans_dict["start_state_q0"]:
+            trans_dict["start_state_q0"][tag] = math.log (float(1)/(line_count+len(tag_dict)))
+
+    for tag1 in end_tag_dict:
+        for tag2 in end_tag_dict:
+            if tag2 not in trans_dict[tag1]:
+                trans_dict[tag1][tag2] = math.log (float(1)/(tag_dict[tag1]+len(tag_dict)))
 
 
 def transition_prob (inner_tag, tag):
@@ -86,9 +97,9 @@ def transition_prob (inner_tag, tag):
         if tag in trans_dict[inner_tag]:
             res = trans_dict[inner_tag][tag]
         else:
-            return math.log((0.01)/line_count)
+            return -1000
     else:
-        return math.log((0.01)/line_count)
+        return -1000
 
     return res
 
@@ -111,10 +122,17 @@ def assign_tag (fdw, words):
 
     word = words[0].strip()
     for tag in end_tag_dict:
-        probability[tag, 0] = transition_prob ("start_state_q0", tag) + emission_prob (tag, word)
+        if word in emission_dict:
+            if tag in emission_dict[word]:
+                probability[tag, 0] = transition_prob("start_state_q0",tag) + emission_dict [word][tag]
+                #print ("%s %s %d %s" % ("start_state_q0", tag, 0, probability[tag,0]))
+            else:
+                probability[tag, 0] = -1000
+        else:
+            probability[tag, 0] = -1000
         backpointer[tag, 0] = "start_state_q0"
 
-
+    #print (probability)
     for t in range (1, len(words)):
         word = words[t].strip()
         if word in emission_dict:
@@ -123,7 +141,7 @@ def assign_tag (fdw, words):
                     max_prob = -999999
                     max_tag = ""
                     for inner_tag in end_tag_dict:
-                        temp = probability [inner_tag, t-1] + transition_prob (inner_tag, tag) + emission_dict[word][tag]
+                        temp = probability[inner_tag, t-1] + transition_prob(inner_tag,tag) + emission_dict[word][tag]
                         #print ("%s %s %d %s" % (inner_tag, tag, t, temp))
                         if temp > max_prob or max_prob == -999999:
                             max_prob = temp
@@ -132,13 +150,23 @@ def assign_tag (fdw, words):
                     probability[tag, t] = max_prob
                     backpointer[tag, t] = max_tag
                 else:
-                    probability[tag, t] = math.log((0.00001)/line_count)
+                    #print ("%s %d %s" % (tag, t, -1000))
+                    probability[tag, t] = -1000
                     backpointer[tag, t] = tag
-
         else:
             for tag in end_tag_dict:
-                probability[tag, t] = math.log((0.00001)/line_count)
-                backpointer[tag, t] = tag
+                #print ("%s %d %s" % (tag, t, -1000))
+                max_prob = -999999
+                max_tag = ""
+                for inner_tag in end_tag_dict:
+                    temp = probability[inner_tag, t-1] + transition_prob(inner_tag,tag)
+                    #print ("%s %s %d %s" % (inner_tag, tag, t, temp))
+                    if temp > max_prob or max_prob == -999999:
+                        max_prob = temp
+                        max_tag = inner_tag
+                #print ("Max: %s %d %s" % (max_tag, t, max_prob))
+                probability[tag, t] = max_prob
+                backpointer[tag, t] = max_tag
 
     max_prob = -999999
     max_tag = ""
@@ -166,6 +194,7 @@ def read_raw_file (fd):
         probability = defaultdict (dict)
         backpointer = defaultdict (dict)
         words = line.split()
+        smoothing()
         assign_tag (fdw, words)
 
 
