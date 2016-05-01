7f75470bc3757d52e7db6259fd08f7714b451368
Sachin Pandit <sachinspandits@gmail.com>
2016-05-01 00:43:04 -0700

    NLP: Assignment8: Reading a file, and storing ngrams in dictionary

--- Assignment8/calculatebleu.py
+++ Assignment8/calculatebleu.py
@@ -7,26 +7,58 @@ from collections import defaultdict
 candidate_input = defaultdict()
 reference_input = defaultdict()
 candidate_count = defaultdict()
-reference_input = defaultdict()
+reference_count = defaultdict()
 candidate_clipped = defaultdict()
 reference_clipped = defaultdict()
+
+def find_ngrams(input_list, n):
+  return zip(*[input_list[i:] for i in range(n)])
 
 def debug_print():
     print candidate_input
     print reference_input
+    print candidate_count
+    print reference_count
+
+
+def store_ngrams (dictionary, line_string):
+    line_list = []
+    line_list = line_string.split()
+    ngrams_list = find_ngrams(line_list, 1)
+    for tuple in ngrams_list:
+        ngrams_string = ""
+        for word in tuple:
+            ngrams_string += word + " "
+        ngrams_string = ngrams_string.strip()
+        if ngrams_string in dictionary:
+            dictionary[ngrams_string] += 1
+        else:
+            dictionary[ngrams_string] = 1
+
+
 
 def read_candidate_file (filename):
+    global candidate_input
+    global candidate_count
+
     with open(filename, "r") as fd:
         index = 0
         for line in fd:
-            candidate_input[index] = str(line).strip()
+            temp_line = str(line).strip()
+            candidate_input[index] = temp_line
+            store_ngrams (candidate_count, temp_line)
             index += 1
 
 def read_reference_file (filename):
+    global reference_input
+    global reference_count
+
     with open(filename, "r") as fd:
         index = 0
         for line in fd:
-            reference_input[index] = str(line).strip()
+            temp_line = str(line).strip()
+            reference_input[index] = temp_line
+            store_ngrams (reference_count, temp_line)
             index += 1
 
 def check_syntax (length):
